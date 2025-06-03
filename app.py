f90ef6197d95c2080e8ade724a8d16bc7a3e68ed
Judit <j.aizpuru@campus.tu-berlin.de>
2025-06-03 20:11:40 +0200

    ask api key

--- app.py
+++ app.py
@@ -14,79 +14,82 @@ st.set_page_config(
         layout="wide"
 )
 
-load_dotenv()
-try:
-    api_key = os.getenv("TOGETHER_API_KEY")
-except:
-    st.text_input(
-        "Input your together API key"
-    )
+#load_dotenv()
 
 DEBUG = False
 
 if __name__ == "__main__":
 
-    st.title("TripAid")
+    st.title("tripAID")
     st.write("")
 
-    col1, col2 = st.columns([3,1])
+    api_key = os.getenv("TOGETHER_API_KEY")
 
-    with col1:
-        location = st.text_input(
-            "Where do you want to travel?",
-            value="Berlin"
+    if api_key is None:
+        api_key = st.text_input(
+            "Input your together API key"
         )
-        start_date = st.date_input(
-            "Start date:",
-            value=datetime.datetime.now()
-        )
-        end_date = st.date_input(
-                "End date:",
-                value=datetime.datetime.now()+datetime.timedelta(days=2)
+
+    if api_key is not None:
+
+        col1, col2 = st.columns([3,1])
+
+        with col1:
+            location = st.text_input(
+                "Where do you want to travel?",
+                value="Berlin"
             )
-
-        nr_days = (end_date-start_date).days
-        weather_forecast = dict()
-        for i in range(nr_days+1):
-            weather_forecast.update(
-                get_weather_forecast_or_history(
-                    location,
-                    start_date + datetime.timedelta(days=i)
+            start_date = st.date_input(
+                "Start date:",
+                value=datetime.datetime.now()
+            )
+            end_date = st.date_input(
+                    "End date:",
+                    value=datetime.datetime.now()+datetime.timedelta(days=2)
                 )
-            )
-    with col2:
-        if location:
-            image = get_image(location)
-            st.image(image)
 
-    llm = ChatTogether(
-        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
-        temperature=0.7,
-        max_tokens=512,
-        max_retries=2,
-        api_key=api_key
-    )
+            nr_days = (end_date-start_date).days
+            weather_forecast = dict()
+            for i in range(nr_days+1):
+                weather_forecast.update(
+                    get_weather_forecast_or_history(
+                        location,
+                        start_date + datetime.timedelta(days=i)
+                    )
+                )
+        with col2:
+            if location:
+                image = get_image(location)
+                st.image(image)
 
-    prompt = f"""
-        You are a travel assistant helping a user plan a holiday in {location}.
-        The user wants to know an itinerary for a {nr_days} vacation.
-        The weather forecast or the historic data for those dates is: {weather_forecast}.
-    
-        Based on the forecast and general plans to do depending on the weather, respond with:
-        1. 3 things to do per day.
-        2. A short explanation why.
-        3. Packing tips per day.
-    
-        Answer:
-    """
+        llm = ChatTogether(
+            model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+            temperature=0.7,
+            max_tokens=512,
+            max_retries=2,
+            api_key=api_key
+        )
 
-    messages = [
-        (
-            "system",
-            prompt
-         )
-    ]
+        prompt = f"""
+            You are a travel assistant helping a user plan a holiday in {location}.
+            The user wants to know an itinerary for a {nr_days} vacation.
+            The weather forecast or the historic data for those dates is: {weather_forecast}.
+        
+            Based on the forecast and general plans to do depending on the weather, respond with:
+            1. 3 things to do per day.
+            2. A short explanation why.
+            3. Packing tips per day.
+        
+            Answer:
+        """
 
-    if st.button("Run planner"):
-        response = llm.invoke(messages)
-        st.markdown(response.content)
+        messages = [
+            (
+                "system",
+                prompt
+             )
+        ]
+
+        if st.button("Run planner"):
+            response = llm.invoke(messages)
+            st.markdown(response.content)
